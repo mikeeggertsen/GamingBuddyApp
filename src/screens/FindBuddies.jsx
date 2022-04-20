4761c29ce5906dfee91968c71b5b3313f4003286
Mike Eggertsen <49395772+keaMike@users.noreply.github.com>
2022-04-20 15:20:03 +0200

    Add FindBuddies and Matches screens

--- src/screens/FindBuddies.jsx
+++ src/screens/FindBuddies.jsx
@@ -1,102 +1,102 @@
+import { useEffect, useState } from 'react';
+import CardBuddy from '../components/cards/CardBuddy';
 import Container from '../components/layout/Container';
-import ScrollList from '../components/lists/ScrollList';
+
+const data = [
+  { id: 1, username: '1', img: './gamer.jpeg' },
+  { id: 2, username: '2', img: './gamer.jpeg' },
+  { id: 3, username: '3', img: './gamer.jpeg' },
+  { id: 4, username: '4', img: './gamer.jpeg' },
+  { id: 5, username: '5', img: './gamer.jpeg' },
+  { id: 6, username: '6', img: './gamer.jpeg' },
+  { id: 7, username: '7', img: './gamer.jpeg' },
+  { id: 8, username: '8', img: './gamer.jpeg' },
+  { id: 9, username: '9', img: './gamer.jpeg' },
+  { id: 10, username: '10', img: './gamer.jpeg' },
+  { id: 11, username: '11', img: './gamer.jpeg' },
+  { id: 12, username: '12', img: './gamer.jpeg' },
+  { id: 13, username: '13', img: './gamer.jpeg' },
+  { id: 14, username: '14', img: './gamer.jpeg' },
+  { id: 15, username: '15', img: './gamer.jpeg' },
+  { id: 16, username: '16', img: './gamer.jpeg' },
+  { id: 17, username: '17', img: './gamer.jpeg' },
+  { id: 18, username: '18', img: './gamer.jpeg' },
+  { id: 19, username: '19', img: './gamer.jpeg' },
+  { id: 20, username: '20', img: './gamer.jpeg' },
+  { id: 21, username: '21', img: './gamer.jpeg' },
+  { id: 22, username: '22', img: './gamer.jpeg' },
+  { id: 23, username: '23', img: './gamer.jpeg' },
+  { id: 24, username: '24', img: './gamer.jpeg' },
+  { id: 25, username: '25', img: './gamer.jpeg' },
+  { id: 26, username: '26', img: './gamer.jpeg' },
+  { id: 27, username: '27', img: './gamer.jpeg' },
+  { id: 28, username: '28', img: './gamer.jpeg' },
+  { id: 29, username: '29', img: './gamer.jpeg' },
+];
+
+const PAGINATION = 10;
 
 export default function FindBuddies() {
-  const buddies = [
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-    { username: 'test', img: './gamer.jpeg' },
-  ];
+  const [buddies, setBuddies] = useState([]);
+  const [canFetch, setCanFetch] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-  function renderPlatformOrGame(item, index) {
-    return (
-      <img key={index} className="h-8 w-8 rounded-md" src={item} alt="game" />
-    );
+  useEffect(() => {
+    fetchBuddies();
+  }, []);
+
+  useEffect(() => {
+    if (!isLoading && buddies.length < 10) {
+      fetchBuddies();
+    }
+  }, [buddies]);
+
+  async function fetchBuddies() {
+    if (!canFetch) return;
+    // fetch buddies
+    const fetchedBuddies = data.slice(0, 10);
+    console.log(fetchedBuddies.length);
+    // tjek om der er flere der kan fetches
+    if (fetchedBuddies.length < PAGINATION) {
+      // set can fetch til false hvis under forventet antal
+      setCanFetch(false);
+    }
+    // tilføj buddies til state
+    setBuddies((prev) => [...prev, ...fetchedBuddies]);
+    setIsLoading(false);
+  }
+
+  function removeBuddy(id) {
+    const filtered = buddies.filter((item) => item.id !== id);
+    setBuddies(filtered);
+  }
+
+  function likeBuddy(id) {
+    const filtered = buddies.filter((item) => item.id !== id);
+    setBuddies(filtered);
   }
 
   function renderBuddy(item, index) {
     return (
-      <div className="h-80 w-64 bg-theme-dark rounded-md relative" key={index}>
-        <div className="absolute top-0 left-0 p-6">
-          <div className="flex flex-row">
-            <img
-              src={item.img}
-              alt="user"
-              className="h-8 w-8 rounded-full mr-2"
-            />
-            <h5 className="text-white">{item.username}</h5>
-          </div>
-          <div>
-            <h3 className="text-white">Bio</h3>
-            <p className="text-xs text-neutral-300">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              aliquid adipisci nesciunt distinctio delectus{' '}
-            </p>
-          </div>
-          <div>
-            <h3 className="text-white">Platforms</h3>
-            <ScrollList
-              className={'gap-2 p-0 my-2'}
-              data={['./platforms/steam.jpg', './platforms/riot.png']}
-              renderItem={renderPlatformOrGame}
-              horizontal
-            />
-          </div>
-          <div>
-            <h3 className="text-white">Games</h3>
-            <ScrollList
-              className={'gap-2 p-0 mt-2'}
-              data={[
-                './games/csgo.jpeg',
-                './games/valorant.jpeg',
-                './games/lol.jpeg',
-              ]}
-              renderItem={renderPlatformOrGame}
-              horizontal
-            />
-          </div>
-        </div>
-      </div>
+      <CardBuddy
+        key={index}
+        buddy={item}
+        likeBuddy={likeBuddy}
+        removeBuddy={removeBuddy}
+      />
     );
   }
 
   return (
     <Container>
-      <h1 className="text-white text-2xl font-medium">Find Buddies</h1>
-      <div className="flex flex-row">
-        <ScrollList
-          className={'flex-wrap'}
-          data={buddies}
-          renderItem={renderBuddy}
-          horizontal
-        />
-      </div>
+      <h1 className="text-white text-2xl font-medium mb-6">Find Buddies</h1>
+      {isLoading ? (
+        <h1 className="text-white text-2xl">Loading...</h1>
+      ) : (
+        <div className="flex flex-row flex-wrap gap-4">
+          {buddies.map(renderBuddy)}
+        </div>
+      )}
     </Container>
   );
 }
--- src/screens/Matches.jsx
+++ src/screens/Matches.jsx
@@ -1,9 +1,33 @@
+import { useState } from 'react';
+import CardBuddy from '../components/cards/CardBuddy';
 import Container from '../components/layout/Container';
 
+const data = [
+  { id: 1, username: '1', img: './gamer.jpeg' },
+  { id: 2, username: '2', img: './gamer.jpeg' },
+  { id: 3, username: '3', img: './gamer.jpeg' },
+  { id: 4, username: '4', img: './gamer.jpeg' },
+  { id: 5, username: '5', img: './gamer.jpeg' },
+];
+
 export default function Matches() {
+  const [buddies, setBuddies] = useState(data);
+
+  function removeBuddy(id) {
+    const filtered = buddies.filter((item) => item.id !== id);
+    setBuddies(filtered);
+  }
+
+  function renderMatch(item, index) {
+    return <CardBuddy key={index} buddy={item} removeBuddy={removeBuddy} />;
+  }
+
   return (
     <Container>
-      <h1 className="text-white text-2xl font-medium">Matches</h1>
+      <h1 className="text-white text-2xl font-medium mb-6">Matches</h1>
+      <div className="flex flex-row flex-wrap gap-4">
+        {buddies.map(renderMatch)}
+      </div>
     </Container>
   );
 }
