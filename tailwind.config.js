e37cf8b952d2a74fe765476c6b1477bec2b4a4d7
mikeeggertsen <m.eggertsen@hotmail.com>
2022-04-20 20:34:53 +0200

    Add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,10 @@ module.exports = {
       colors: {
         'theme-blue': '#4EA2FF',
         'theme-green': '#02C39A',
+        'theme-green-dark': '#029979',
         'theme-light-dark': '#1B1F24',
         'theme-dark': '#121418',
+        'theme-black-transparent': 'rgba(0, 0, 0, 0.5);',
       },
       minWidth: {
         48: '12rem',
