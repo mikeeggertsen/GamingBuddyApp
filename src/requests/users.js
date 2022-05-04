1a8a1b2f95e739891101d4378ac3186c7ded1218
mikeeggertsen <m.eggertsen@hotmail.com>
2022-05-04 16:40:15 +0200

    Add update user functionality

--- src/requests/users.js
+++ src/requests/users.js
@@ -1,8 +1,17 @@
-import { apiGet } from './client';
+import { apiGet, apiPut } from './client';
 
 export async function getOwnUser() {
   try {
     return await apiGet('/api/users/own');
+  } catch (error) {
+    console.log('Failed fetching own user', error);
+    throw error;
+  }
+}
+
+export async function updateUser(user) {
+  try {
+    return await apiPut(user, '/api/users/updateuser');
   } catch (error) {
     console.log('Failed fetching own user', error);
     throw error;
--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -2,41 +2,46 @@ import { faAdd } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { useRecoilState } from 'recoil';
 import CardPlatformGame from '../components/cards/CardPlatformGame';
 import Container from '../components/layout/Container';
 import ScrollList from '../components/lists/ScrollList';
 import { ModalAdd } from '../components/modals/Modals';
+import { userState } from '../recoil/atoms/user';
+import { updateUser } from '../requests/users';
 
 export default function Profile() {
-  const [user, setUser] = useState();
+  const [user, setUser] = useRecoilState(userState);
+  const [updateState, setUpdateState] = useState(user);
+
+  const [games, setGames] = useState([
+    { image: './games/csgo.jpeg' },
+    { image: './games/dota2.jpeg' },
+    { image: './games/fifa22.jpeg' },
+  ]);
+  const [platforms, setPlatforms] = useState([
+    { image: './platforms/steam.jpg' },
+    { image: './platforms/origin.png', background: 'white' },
+    { image: './platforms/playstation.jpeg' },
+  ]);
   const [showAddPlatform, setShowAddPlatform] = useState(false);
   const [showAddGame, setShowAddGame] = useState(false);
 
   useEffect(() => {
-    //fetch user
-    setUser({
-      username: 'GamerDude',
-      bio: 'Im a gamer',
-      img: 'gamer.jpeg',
-      platforms: [
-        { image: './platforms/steam.jpg' },
-        { image: './platforms/origin.png', background: 'white' },
-        { image: './platforms/playstation.jpeg' },
-      ],
-      games: [
-        { image: './games/csgo.jpeg' },
-        { image: './games/dota2.jpeg' },
-        { image: './games/fifa22.jpeg' },
-      ],
-    });
+    //fetch games and platforms
   }, []);
 
-  function saveProfile() {
-    console.log('Saving profile');
+  useEffect(() => {
+    setUpdateState(user);
+  }, [user]);
+
+  async function saveProfile() {
+    const updatedUser = await updateUser(updateState);
+    setUser(updatedUser);
   }
 
-  function setUserProperty(property) {
-    setUser({ ...user, ...property });
+  function setUpdateProperty(property) {
+    setUpdateState({ ...user, ...property });
   }
 
   function addPlatform(event) {
@@ -67,7 +72,7 @@ export default function Profile() {
           <div className="flex flex-col items-center">
             <img
               className="h-32 w-32 rounded-full"
-              src={user.img}
+              src={'gamer.jpeg'}
               alt="avatar"
             />
           </div>
@@ -75,17 +80,40 @@ export default function Profile() {
             <input
               className="rounded-md px-4 py-2 my-6"
               type={'text'}
-              value={user.username}
+              value={updateState.username}
               placeholder="Username"
-              onChange={(e) => setUserProperty({ username: e.target.value })}
+              onChange={(e) => setUpdateProperty({ username: e.target.value })}
             />
             <textarea
               className="rounded-md px-4 py-2 mb-6"
               type={'text'}
-              value={user.bio}
+              value={updateState.bio}
               placeholder="Bio"
               rows={6}
-              onChange={(e) => setUserProperty({ bio: e.target.value })}
+              onChange={(e) => setUpdateProperty({ bio: e.target.value })}
+            />
+            <input
+              className="rounded-md px-4 py-2 mb-6"
+              type={'text'}
+              value={updateState.email}
+              placeholder="Email"
+              onChange={(e) => setUpdateProperty({ email: e.target.value })}
+            />
+            <input
+              className="rounded-md px-4 py-2 mb-6"
+              type={'password'}
+              value={updateState.password}
+              placeholder="Password"
+              onChange={(e) => setUpdateProperty({ password: e.target.value })}
+            />
+            <input
+              className="rounded-md px-4 py-2 mb-6"
+              type={'password'}
+              value={updateState.repeatPassword}
+              placeholder="Repeat password"
+              onChange={(e) =>
+                setUpdateProperty({ repeatPassword: e.target.value })
+              }
             />
             {/** PLATFORM LIST */}
             <div className="flex items-center justify-between">
@@ -98,11 +126,7 @@ export default function Profile() {
                 />
               </button>
             </div>
-            <ScrollList
-              data={user.platforms}
-              renderItem={renderCard}
-              horizontal
-            />
+            <ScrollList data={platforms} renderItem={renderCard} horizontal />
             {/** GAME LIST */}
             <div className="flex items-center justify-between">
               <h3 className="text-white text-2xl">Games</h3>
@@ -114,7 +138,7 @@ export default function Profile() {
                 />
               </button>
             </div>
-            <ScrollList data={user.games} renderItem={renderCard} horizontal />
+            <ScrollList data={games} renderItem={renderCard} horizontal />
           </div>
         </>
       )}
