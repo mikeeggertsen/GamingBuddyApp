e89ae1910e8beec38f17cd43420060d0fbd815c7
mikeeggertsen <m.eggertsen@hotmail.com>
2022-05-04 14:18:33 +0200

    Add protected routes

--- src/App.js
+++ src/App.js
@@ -7,19 +7,29 @@ import FindBuddies from './screens/FindBuddies';
 import Matches from './screens/Matches';
 import Inbox from './screens/Inbox';
 import Profile from './screens/Profile';
+import LandingPage from './screens/LandingPage';
 
 export default function App() {
   const [open, setOpen] = useState(true);
+  const token = localStorage.getItem('token');
 
   return (
     <SidebarContext.Provider value={{ open, setOpen }}>
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Dashboard />} />
-          <Route exact path="/findbuddies" element={<FindBuddies />} />
-          <Route exact path="/matches" element={<Matches />} />
-          <Route exact path="/inbox" element={<Inbox />} />
-          <Route exact path="/profile" element={<Profile />} />
+          {token ? (
+            <>
+              <Route exact path="/" element={<Dashboard />} />
+              <Route exact path="/findbuddies" element={<FindBuddies />} />
+              <Route exact path="/matches" element={<Matches />} />
+              <Route exact path="/inbox" element={<Inbox />} />
+              <Route exact path="/profile" element={<Profile />} />
+            </>
+          ) : (
+            <>
+              <Route exact path="/" element={<LandingPage />} />
+            </>
+          )}
         </Routes>
       </BrowserRouter>
     </SidebarContext.Provider>
