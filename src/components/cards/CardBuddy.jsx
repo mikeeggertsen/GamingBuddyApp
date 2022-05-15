67688fffb813b913c2fdd8d25cdf77724f51fdd1
mikeeggertsen <m.eggertsen@hotmail.com>
2022-05-15 18:32:12 +0200

    Refactor CardBuddy

--- src/components/cards/CardBuddy.jsx
+++ src/components/cards/CardBuddy.jsx
@@ -5,11 +5,21 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ScrollList from '../lists/ScrollList';
+import { gameImageParser, platformImageParser } from '../../utils/imageParser';
 
-export default function CardBuddy({ buddy, removeBuddy, likeBuddy }) {
+export default function CardBuddy({ buddy, swipe, isMatch }) {
   function renderPlatformOrGame(item, index) {
     return (
-      <img key={index} className="h-8 w-8 rounded-md" src={item} alt="game" />
+      <img
+        key={index}
+        className={`h-8 w-8 rounded-md ${
+          buddy.platforms.find(({ platform }) => platform === 'Origin')
+            ? 'bg-white'
+            : ''
+        }`}
+        src={item}
+        alt="game"
+      />
     );
   }
 
@@ -17,44 +27,51 @@ export default function CardBuddy({ buddy, removeBuddy, likeBuddy }) {
     <div className="h-96 w-full bg-theme-dark rounded-md flex flex-col justify-between p-4">
       <div className="flex flex-row items-center">
         <img
-          src={buddy.img}
+          src={'/gamer.jpeg'}
           alt="user"
           className="h-12 w-12 rounded-full mr-2"
         />
-        <h5 className="text-white text-xl font-medium">{buddy.username}</h5>
+        <h5 className="text-white text-xl font-medium line-clamp-2">
+          {buddy.username}
+        </h5>
       </div>
       <div className="my-2">
         <h3 className="text-white font-medium">Bio</h3>
-        <p className="text-xs text-neutral-300 line-clamp-3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-          aliquid adipisci nesciunt distinctio delectus Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Mollitia aliquid adipisci nesciunt
-          distinctio delectus
-        </p>
+        <p className="text-xs text-neutral-300 line-clamp-3">{buddy.bio}</p>
       </div>
       <div>
         <h3 className="text-white font-medium">Platforms</h3>
-        <ScrollList
-          className={'gap-[1rem]'}
-          data={['./platforms/steam.jpg', './platforms/riot.png']}
-          renderItem={renderPlatformOrGame}
-          horizontal
-        />
+        {buddy.platforms?.length > 0 ? (
+          <ScrollList
+            className={'gap-[1rem]'}
+            data={buddy.platforms.map(({ platform }) =>
+              platformImageParser(platform),
+            )}
+            renderItem={renderPlatformOrGame}
+            horizontal
+          />
+        ) : (
+          <p className="text-xs text-neutral-300 my-1">
+            {buddy.username} is not on any platforms
+          </p>
+        )}
         <h3 className="text-white font-medium">Games</h3>
-        <ScrollList
-          className={'gap-[1rem]'}
-          data={[
-            './games/csgo.jpeg',
-            './games/valorant.jpeg',
-            './games/lol.jpeg',
-          ]}
-          renderItem={renderPlatformOrGame}
-          horizontal
-        />
+        {buddy.games?.length > 0 ? (
+          <ScrollList
+            className={'gap-[1rem]'}
+            data={buddy.games.map(({ name }) => gameImageParser(name))}
+            renderItem={renderPlatformOrGame}
+            horizontal
+          />
+        ) : (
+          <p className="text-xs text-neutral-300 mt-1">
+            {buddy.username} is not playing any games
+          </p>
+        )}
       </div>
       <div className="flex flex-row justify-around">
         <div
-          onClick={() => removeBuddy(buddy.id)}
+          onClick={() => swipe(buddy.id, false)}
           className="bg-red-500 rounded-full w-12 h-12 flex items-center justify-center cursor-pointer"
         >
           <FontAwesomeIcon
@@ -63,9 +80,9 @@ export default function CardBuddy({ buddy, removeBuddy, likeBuddy }) {
             size={'lg'}
           />
         </div>
-        {likeBuddy ? (
+        {!isMatch ? (
           <div
-            onClick={() => likeBuddy(buddy.id)}
+            onClick={() => swipe(buddy.id, true)}
             className="bg-theme-green rounded-full w-12 h-12 flex items-center justify-center cursor-pointer"
           >
             <FontAwesomeIcon
