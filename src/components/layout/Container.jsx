81e685a197463544832bf702cee89ecd23164e64
mikeeggertsen <m.eggertsen@hotmail.com>
2022-05-15 18:32:41 +0200

    Minor refactor to Container

--- src/components/layout/Container.jsx
+++ src/components/layout/Container.jsx
@@ -1,14 +1,13 @@
 import Chatbar from '../bars/Chatbar';
 import Sidebar from '../bars/Sidebar';
 
-export default function Container({ children, showActivies }) {
+export default function Container({ children }) {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="h-screen w-screen overflow-auto px-4 lg:px-20 py-8 bg-theme-light-dark">
+      <div className="h-screen w-screen overflow-hidden px-4 lg:px-20 py-8 bg-theme-light-dark">
         {children}
       </div>
-      <Chatbar />
     </div>
   );
 }
