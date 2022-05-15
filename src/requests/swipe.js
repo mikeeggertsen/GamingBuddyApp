fd733e60a01257a04d5929a0b17005c3e20b8894
mikeeggertsen <m.eggertsen@hotmail.com>
2022-05-15 18:35:00 +0200

    Implement infinite list on Matches

---
+++ src/requests/swipe.js
@@ -0,0 +1,22 @@
+import { apiGet, apiPost } from './client';
+
+export async function getMatches(skip, limit) {
+  try {
+    const res = await apiGet(`/api/swipes/matches?skip=${skip}&limit=${limit}`);
+    console.log(res.data);
+    return res.data;
+  } catch (error) {
+    console.log('Failed fetching matches', error);
+    throw error;
+  }
+}
+
+export async function swipeOnUser(otherUserId, status) {
+  try {
+    const res = await apiPost({ otherUserId, status }, '/api/swipes/swipe');
+    return res.data;
+  } catch (error) {
+    console.log('Failed swiping on user', error);
+    throw error;
+  }
+}
--- src/screens/Matches.jsx
+++ src/screens/Matches.jsx
@@ -1,33 +1,54 @@
 import { useState } from 'react';
 import CardBuddy from '../components/cards/CardBuddy';
 import Container from '../components/layout/Container';
+import InfiniteScrollList from '../components/lists/InfiniteScrollList';
+import { getMatches, swipeOnUser } from '../requests/swipe';
 
-const data = [
-  { id: 1, username: '1', img: './gamer.jpeg' },
-  { id: 2, username: '2', img: './gamer.jpeg' },
-  { id: 3, username: '3', img: './gamer.jpeg' },
-  { id: 4, username: '4', img: './gamer.jpeg' },
-  { id: 5, username: '5', img: './gamer.jpeg' },
-];
+const LIMIT = 10;
 
 export default function Matches() {
-  const [buddies, setBuddies] = useState(data);
+  const [buddies, setBuddies] = useState([]);
 
-  function removeBuddy(id) {
+  async function fetchBuddies(skip) {
+    return await getMatches(skip, LIMIT);
+  }
+
+  async function swipe(id, status) {
+    await swipeOnUser(id, status);
     const filtered = buddies.filter((item) => item.id !== id);
     setBuddies(filtered);
   }
 
-  function renderMatch(item, index) {
-    return <CardBuddy key={index} buddy={item} removeBuddy={removeBuddy} />;
+  function renderMatch(item) {
+    return <CardBuddy key={item.id} buddy={item} swipe={swipe} isMatch />;
+  }
+
+  function renderNoMatches() {
+    return (
+      <div className="flex flex-col h-full w-full justify-center items-center">
+        <h2 className="text-white text-xl font-normal">
+          You don't have any matches
+        </h2>
+        <p className="text-white text-xs font-normal mt-2">
+          So what are you waiting for? Find your Gaming Buddy!
+        </p>
+        <img className="w-2/3 h-2/3" src="/findbuddies.svg" alt="No matches" />
+      </div>
+    );
   }
 
   return (
     <Container>
-      <h1 className="text-white text-2xl font-medium mb-6">Matches</h1>
-      <div className="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {buddies.map(renderMatch)}
-      </div>
+      <h1 className="text-white text-2xl font-medium mb-4">Matches</h1>
+      <InfiniteScrollList
+        className="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pb-12 h-full overflow-auto"
+        items={buddies}
+        setItems={setBuddies}
+        getData={fetchBuddies}
+        renderItem={renderMatch}
+        renderEmptyListComponent={renderNoMatches}
+        pagination={LIMIT}
+      />
     </Container>
   );
 }
