91ae010164c49201bee0eae13230ef637a2b8b54
mikeeggertsen <m.eggertsen@hotmail.com>
2022-05-18 09:23:37 +0200

    Refactor profile and add integration

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,168 +1,233 @@
 import { faAdd } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useRef } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useRecoilState } from 'recoil';
 import CardPlatformGame from '../components/cards/CardPlatformGame';
 import Container from '../components/layout/Container';
 import ScrollList from '../components/lists/ScrollList';
-import { ModalAdd } from '../components/modals/Modals';
+import { ModalAddPlatform, ModalAddGame } from '../components/modals/Modals';
 import { userState } from '../recoil/atoms/user';
-import { updateUser } from '../requests/users';
+import {
+  addGameToProfile,
+  addPlatformToProfile,
+  updateUser,
+} from '../requests/users';
+import { gameImageParser, platformImageParser } from '../utils/imageParser';
 
 export default function Profile() {
   const [user, setUser] = useRecoilState(userState);
   const [updateState, setUpdateState] = useState(user);
-
-  const [games, setGames] = useState([
-    { image: './games/csgo.jpeg' },
-    { image: './games/dota2.jpeg' },
-    { image: './games/fifa22.jpeg' },
-  ]);
-  const [platforms, setPlatforms] = useState([
-    { image: './platforms/steam.jpg' },
-    { image: './platforms/origin.png', background: 'white' },
-    { image: './platforms/playstation.jpeg' },
-  ]);
   const [showAddPlatform, setShowAddPlatform] = useState(false);
   const [showAddGame, setShowAddGame] = useState(false);
-
-  useEffect(() => {
-    //fetch games and platforms
-  }, []);
+  const [prompt, setPrompt] = useState('');
+  let promptTimeout = useRef();
 
   useEffect(() => {
     setUpdateState(user);
   }, [user]);
 
+  useEffect(() => {
+    clearTimeout(promptTimeout.current);
+    if (prompt) {
+      promptTimeout.current = setTimeout(() => {
+        setPrompt('');
+      }, 3000);
+    }
+  }, [prompt]);
+
   async function saveProfile() {
-    const updatedUser = await updateUser(updateState);
-    setUser(updatedUser);
+    const { password, repeatPassword } = updateState;
+    if (password !== repeatPassword) {
+      setPrompt({ error: true, msg: 'Passwords does not match' });
+      return;
+    }
+    await updateUser(updateState);
+    setUser({ ...updateState, password: '', repeatPassword: '' });
+    setPrompt({ error: false, msg: 'Your profile has been updated' });
   }
 
   function setUpdateProperty(property) {
-    setUpdateState({ ...user, ...property });
+    setUpdateState({ ...updateState, ...property });
   }
 
-  function addPlatform(event) {
+  async function addPlatform(event) {
     event.preventDefault();
     setShowAddPlatform(false);
-    console.log('Adding platform', event.target[0].value);
-    event.target[0].value = '';
+    const { selectedIndex } = event.target[0].options;
+    const { value, text } = event.target[0].options[selectedIndex];
+    const id = Number(value);
+    if (id === 0 || user.platforms?.find((item) => item.id === id)) {
+      return;
+    }
+    await addPlatformToProfile(id);
+    setUpdateProperty({
+      platforms: [...updateState.platforms, { id, platform: text }],
+    });
+    event.target[0].value = 0;
   }
 
-  function addGame(event) {
+  async function addGame(event) {
     event.preventDefault();
-    setShowAddPlatform(false);
-    console.log('Adding game', event.target[0].value);
-    event.target[0].value = '';
+    setShowAddGame(false);
+    const { selectedIndex } = event.target[0].options;
+    const { value, text } = event.target[0].options[selectedIndex];
+    const id = Number(value);
+    if (id === 0 || user.games?.find((item) => item.id === id)) {
+      return;
+    }
+    await addGameToProfile(id);
+    setUpdateProperty({
+      games: [...updateState.games, { id, name: text }],
+    });
+    event.target[0].value = 0;
   }
 
-  function renderCard(item, index) {
-    return <CardPlatformGame key={index} item={item} />;
+  function renderGameCard(item) {
+    const game = { ...item };
+    game.image = gameImageParser(item.name);
+    return <CardPlatformGame key={game.id} item={game} />;
+  }
+
+  function renderPlatformCard(item) {
+    const platform = { ...item };
+    platform.image = platformImageParser(item.platform);
+    return <CardPlatformGame key={item.id} item={platform} />;
+  }
+
+  function EmptyCard({ text }) {
+    return (
+      <div className="border border-white p-6 flex items-center rounded-md min-w-48 w-48 h-64">
+        <p className="text-white text-center text-xs">{text}</p>
+      </div>
+    );
+  }
+
+  function renderEmptyGame() {
+    return <EmptyCard text={'Add new games to find buddies to play with'} />;
+  }
+
+  function renderEmptyPlatform() {
+    return (
+      <EmptyCard text={'Add new platforms to find buddies to play with'} />
+    );
   }
 
   return (
     <Container>
-      <h1 className="text-white text-2xl p-2">Profile</h1>
-      {!user ? (
-        <h1 className="text-white text-2xl p-2">Loading...</h1>
-      ) : (
-        <>
-          <div className="flex flex-col items-center">
-            <img
-              className="h-32 w-32 rounded-full"
-              src={'gamer.jpeg'}
-              alt="avatar"
-            />
+      <div className="w-full h-full overflow-y-scroll pb-8 relative">
+        {prompt && (
+          <div
+            className={`${
+              prompt.error ? 'bg-red-400' : 'bg-theme-green'
+            } fixed bottom-4 text-white text-center text-14 w-2/3 py-4 left-1/2 -translate-x-1/2 rounded-md z-10`}
+          >
+            <p>{prompt.msg}</p>
           </div>
-          <div className="flex flex-col">
-            <input
-              className="rounded-md px-4 py-2 my-6"
-              type={'text'}
-              value={updateState.username}
-              placeholder="Username"
-              onChange={(e) => setUpdateProperty({ username: e.target.value })}
-            />
-            <textarea
-              className="rounded-md px-4 py-2 mb-6"
-              type={'text'}
-              value={updateState.bio}
-              placeholder="Bio"
-              rows={6}
-              onChange={(e) => setUpdateProperty({ bio: e.target.value })}
-            />
-            <input
-              className="rounded-md px-4 py-2 mb-6"
-              type={'text'}
-              value={updateState.email}
-              placeholder="Email"
-              onChange={(e) => setUpdateProperty({ email: e.target.value })}
-            />
-            <input
-              className="rounded-md px-4 py-2 mb-6"
-              type={'password'}
-              value={updateState.password}
-              placeholder="Password"
-              onChange={(e) => setUpdateProperty({ password: e.target.value })}
-            />
-            <input
-              className="rounded-md px-4 py-2 mb-6"
-              type={'password'}
-              value={updateState.repeatPassword}
-              placeholder="Repeat password"
-              onChange={(e) =>
-                setUpdateProperty({ repeatPassword: e.target.value })
-              }
-            />
-            {/** PLATFORM LIST */}
-            <div className="flex items-center justify-between">
-              <h3 className="text-white text-2xl">Platforms</h3>
-              <button className="text-white text-md bg-theme-green w-8 rounded-md h-fit hover:bg-theme-green-dark duration-300">
-                <FontAwesomeIcon
+        )}
+        <h1 className="text-white text-2xl p-2">Profile</h1>
+        {!user ? (
+          <h1 className="text-white text-2xl p-2">Loading...</h1>
+        ) : (
+          <>
+            <div className="flex flex-col items-center">
+              <img
+                className="h-32 w-32 rounded-full"
+                src={'gamer.jpeg'}
+                alt="avatar"
+              />
+            </div>
+            <div className="flex flex-col">
+              <input
+                className="rounded-md px-4 py-2 my-6"
+                type={'text'}
+                value={updateState.username}
+                placeholder="Username"
+                onChange={(e) =>
+                  setUpdateProperty({ username: e.target.value })
+                }
+              />
+              <textarea
+                className="rounded-md px-4 py-2 mb-6"
+                type={'text'}
+                value={updateState.bio ?? ''}
+                placeholder="Bio"
+                rows={6}
+                onChange={(e) => setUpdateProperty({ bio: e.target.value })}
+              />
+              <input
+                className="rounded-md px-4 py-2 mb-6"
+                type={'password'}
+                value={updateState.password}
+                placeholder="Password"
+                onChange={(e) =>
+                  setUpdateProperty({ password: e.target.value })
+                }
+              />
+              <input
+                className="rounded-md px-4 py-2 mb-6"
+                type={'password'}
+                value={updateState.repeatPassword}
+                placeholder="Repeat password"
+                onChange={(e) =>
+                  setUpdateProperty({ repeatPassword: e.target.value })
+                }
+              />
+              <div className="flex flex-col items-center justify-center">
+                <button
+                  onClick={saveProfile}
+                  className="text-white bg-theme-green hover:bg-theme-green-dark duration-300 w-32 py-2 rounded-md"
+                >
+                  Save
+                </button>
+              </div>
+              {/** PLATFORM LIST */}
+              <div className="flex items-center justify-between mb-2">
+                <h3 className="text-white text-2xl">Platforms</h3>
+                <button
                   onClick={() => setShowAddPlatform(true)}
-                  icon={faAdd}
-                  size="sm"
-                />
-              </button>
-            </div>
-            <ScrollList data={platforms} renderItem={renderCard} horizontal />
-            {/** GAME LIST */}
-            <div className="flex items-center justify-between">
-              <h3 className="text-white text-2xl">Games</h3>
-              <button className="text-white text-md bg-theme-green w-8 rounded-md h-fit hover:bg-theme-green-dark duration-300">
-                <FontAwesomeIcon
+                  className="text-white text-md bg-theme-green w-8 rounded-md h-fit hover:bg-theme-green-dark duration-300"
+                >
+                  <FontAwesomeIcon icon={faAdd} size="sm" />
+                </button>
+              </div>
+              <ScrollList
+                data={updateState.platforms}
+                renderItem={renderPlatformCard}
+                horizontal
+                emptyListComponent={renderEmptyPlatform}
+              />
+              <br />
+              {/** GAME LIST */}
+              <div className="flex items-center justify-between mb-2">
+                <h3 className="text-white text-2xl">Games</h3>
+                <button
                   onClick={() => setShowAddGame(true)}
-                  icon={faAdd}
-                  size="sm"
-                />
-              </button>
+                  className="text-white text-md bg-theme-green w-8 rounded-md h-fit hover:bg-theme-green-dark duration-300"
+                >
+                  <FontAwesomeIcon icon={faAdd} size="sm" />
+                </button>
+              </div>
+              <ScrollList
+                data={updateState.games}
+                renderItem={renderGameCard}
+                horizontal
+                emptyListComponent={renderEmptyGame}
+              />
             </div>
-            <ScrollList data={games} renderItem={renderCard} horizontal />
-          </div>
-        </>
-      )}
-      <ModalAdd
-        title={'Add Platform'}
-        placeholder={'Platform'}
-        onSubmit={addPlatform}
-        visible={showAddPlatform}
-        onClose={() => setShowAddPlatform(false)}
-      />
-      <ModalAdd
-        title={'Add Game'}
-        placeholder={'Game'}
-        onSubmit={addGame}
-        visible={showAddGame}
-        onClose={() => setShowAddGame(false)}
-      />
-      <div className="flex flex-col items-center justify-center mt-6">
-        <button
-          onClick={saveProfile}
-          className="text-white bg-theme-green hover:bg-theme-green-dark duration-300 w-32 py-2 rounded-md"
-        >
-          Save
-        </button>
+          </>
+        )}
+        <ModalAddPlatform
+          onSubmit={addPlatform}
+          visible={showAddPlatform}
+          onClose={() => setShowAddPlatform(false)}
+        />
+        <ModalAddGame
+          onSubmit={addGame}
+          visible={showAddGame}
+          onClose={() => setShowAddGame(false)}
+        />
       </div>
     </Container>
   );
