8c855ceddb035274afacee4c1e382bb9a4da1875
mikeeggertsen <m.eggertsen@hotmail.com>
2022-05-18 09:22:43 +0200

    Add game and platform to profile funcs

--- src/requests/users.js
+++ src/requests/users.js
@@ -25,11 +25,32 @@ export async function getOwnUser() {
   }
 }
 
+export async function addGameToProfile(gameId) {
+  try {
+    return await apiPut({ game: { gameId } }, '/api/users/addgametouser');
+  } catch (error) {
+    console.log('Failed adding game', error);
+    throw error;
+  }
+}
+
+export async function addPlatformToProfile(platformId) {
+  try {
+    return await apiPut(
+      { platform: { platformId } },
+      '/api/users/addplatformtouser',
+    );
+  } catch (error) {
+    console.log('Failed adding platform', error);
+    throw error;
+  }
+}
+
 export async function updateUser(user) {
   try {
     return await apiPut(user, '/api/users/updateuser');
   } catch (error) {
-    console.log('Failed fetching own user', error);
+    console.log('Failed updating user', error);
     throw error;
   }
 }
