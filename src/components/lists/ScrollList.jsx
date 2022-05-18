f18a336cac652c1efcbc6b3b656d91ded70cf3b3
mikeeggertsen <m.eggertsen@hotmail.com>
2022-05-18 09:21:08 +0200

    Add empty list component to ScrollList

--- src/components/lists/ScrollList.jsx
+++ src/components/lists/ScrollList.jsx
@@ -3,14 +3,19 @@ export default function ScrollList({
   renderItem,
   horizontal,
   className,
+  emptyListComponent,
 }) {
+  if (!data || data?.length === 0) {
+    return emptyListComponent();
+  }
+
   return (
     <ul
       className={`${className} flex ${
         horizontal ? 'flex-row' : 'flex-col'
       } gap-6 overflow-x-auto p-2`}
     >
-      {data.map((item, index) => renderItem(item, index))}
+      {data?.map((item, index) => renderItem(item, index))}
     </ul>
   );
 }
