f86f771169269fa457e091e193f1c921d889e552
mikeeggertsen <m.eggertsen@hotmail.com>
2022-05-18 09:46:35 +0200

    Add conditional scroll to Container

--- src/components/layout/Container.jsx
+++ src/components/layout/Container.jsx
@@ -1,11 +1,18 @@
-import Chatbar from '../bars/Chatbar';
+import { useDimensions } from '../../hooks/useDimensions';
+import Bottombar from '../bars/Bottombar';
 import Sidebar from '../bars/Sidebar';
 
-export default function Container({ children }) {
+export default function Container({ children, scroll }) {
+  const dimensions = useDimensions();
+  const isMobile = dimensions?.width < 1024;
   return (
     <div className="flex">
-      <Sidebar />
-      <div className="h-screen w-screen overflow-hidden px-4 lg:px-20 pt-8 bg-theme-light-dark">
+      {isMobile ? <Bottombar /> : <Sidebar />}
+      <div
+        className={`h-screen w-screen overflow-hidden px-4 lg:px-20 pt-8 pb-24 md:pb-8 bg-theme-light-dark ${
+          scroll && 'overflow-y-scroll'
+        }`}
+      >
         {children}
       </div>
     </div>
