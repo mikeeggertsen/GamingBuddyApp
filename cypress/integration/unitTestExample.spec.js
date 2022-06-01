95620fe24180b7940bb093a48cadbdded924f785
Asbjørn Stahl Garne <asbjoern@garne.dk>
2022-06-01 18:27:20 +0200

    added example of a unit test in Cypress

---
+++ cypress/integration/unitTestExample.spec.js
@@ -0,0 +1,7 @@
+import { Paths } from '../../src/constants/Paths.js'
+
+describe('Example of a unit test', () => {
+    it('is working', () => {
+        expect(Paths['API_URL']).to.equal('https://gb.ryholt.xyz')
+    })
+}) 
